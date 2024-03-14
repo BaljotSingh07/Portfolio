74a38498f14134fad825c1c91e1713fba48577e5
Baljot Singh <baljotvirk07@gmail.com>
2024-03-14 12:38:33 -0500

    intro changed, line added

---
+++ script.js
@@ -0,0 +1,14 @@
+new LeaderLine(document.getElementById("sub"), document.getElementById("skills"), {
+     color: "white", size: 3, path: "grid", startSocket: "bottom", endPlug: "behind", 
+     dropShadow: { color: "white", dx: 20, dy: 20, blur: 0 },
+     middleLabel: LeaderLine.pathLabel({text: "More about me", outlineColor: ""}) });
+
+new LeaderLine(document.getElementById("sub"), document.getElementById("skills"), { color: "white", size: 3, path: "grid", startSocket: "bottom", endPlug: "behind", dropShadow: { color: "white", dx: 40, dy: 40, blur: 2 } });
+
+new LeaderLine(document.getElementById("skills"), document.getElementById("projects-header"), { color: "white", size: 3, path: "grid", startSocket: "bottom", endPlug: "behind", dropShadow: { color: "white", dx: 20, dy: 20, blur: 0 } });
+
+new LeaderLine(document.getElementById("skills"), document.getElementById("projects-header"), { color: "white", size: 3, path: "grid", startSocket: "bottom", endPlug: "behind", dropShadow: { color: "white", dx: 40, dy: 40, blur: 0 } });
+
+new LeaderLine(document.getElementsByClassName("project")[0], document.getElementsByClassName("project")[1], { color: "white", size: 1, path: "grid", startSocket: "left", endSocket: "left", endPlug: "behind" });
+
+new LeaderLine(document.getElementsByClassName("project")[1], document.getElementsByClassName("project")[2], { color: "white", size: 1, path: "grid", startSocket: "right", endSocket: "right", endPlug: "behind" });
