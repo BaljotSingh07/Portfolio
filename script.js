1a268769557478d838105a8dd7b4c6b933dab258
Baljot Singh <baljotvirk07@gmail.com>
2024-03-14 16:18:29 -0500

    fixed nav mobile issue, delete unused images

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ new LeaderLine(document.getElementById("sub"), document.getElementById("skills")
 
 new LeaderLine(document.getElementById("skills"), document.getElementById("projects-header"), { color: "white", size: 3, path: "grid", startSocket: "bottom", endPlug: "behind", dropShadow: { color: "white", dx: 20, dy: 20, blur: 0 } });
 
-new LeaderLine(document.getElementById("skills"), document.getElementById("projects-header"), { color: "white", size: 3, path: "grid", startSocket: "bottom", endPlug: "behind", dropShadow: { color: "white", dx: 40, dy: 40, blur: 0 } });
+new LeaderLine(document.getElementById("skills"), document.getElementById("projects-header"), { color: "white", size: 3, path: "grid", startSocket: "bottom", endPlug: "behind", dropShadow: { color: "white", dx: 40, dy: 40, blur: 2 } });
 
 new LeaderLine(document.getElementsByClassName("project")[0], document.getElementsByClassName("project")[1], { color: "white", size: 1, path: "grid", startSocket: "left", endSocket: "left", endPlug: "behind" });
 
